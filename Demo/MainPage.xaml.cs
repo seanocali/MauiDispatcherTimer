2183b2ad5248b3197d575c93c63d356b92e351d8
Sean O'Neil <seanoneilcalifornia@gmail.com>
2022-08-17 08:46:20 -0700

    Add project files.

---
+++ Demo/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MauiDispatcherTimer;
+
+namespace Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += Timer_Tick;
+            InitializeComponent();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tickLabel.Text = $"Tick at {DateTime.Now.ToString("O")}";
+        }
+
+        DispatcherTimer timer;
+
+        private void StartButton_Clicked(object sender, EventArgs e)
+        {
+            headerLabel.Text = "Running, 1s Interval";
+            timer.Start();
+        }
+
+        private void StopButton_Clicked(object sender, EventArgs e)
+        {
+            timer.Stop();
+            headerLabel.Text = "Stopped";
+            tickLabel.Text = "";
+        }
+    }
+}
---
+++ MauiDispatcherTimer/DispatcherTimer.cs
@@ -0,0 +1,121 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace MauiDispatcherTimer
+{
+    /// <summary>
+    /// Abstraction of MAUI's PeriodicTimer to mimic the DispatcherTimer 
+    /// found in System.Windows.Threading and Windows.UI.Xaml.
+    /// </summary>
+    public class DispatcherTimer : IDispatcherTimer
+    {
+        PeriodicTimer _timer;
+        CancellationTokenSource _cts;
+
+
+        // Returns:
+        //     The amount of time between ticks. The default is a **TimeSpan**
+        TimeSpan _interval = new TimeSpan();
+        public TimeSpan Interval
+        {
+            get => _interval;
+            set
+            {
+                if (value != _interval)
+                {
+                    _interval = value;
+                    if (_isEnabled)
+                    {
+                        Start();
+                    }
+                }
+            }
+        }
+
+        //
+        // Summary:
+        //     Gets a value that indicates whether the timer is running.
+        //
+        // Returns:
+        //     **true** if the timer is enabled and running; otherwise, **false**.
+        bool _isEnabled;
+        public bool IsEnabled => _isEnabled;
+        public bool IsRunning => _isEnabled;
+
+
+        bool _isRepeating = true;
+        public bool IsRepeating
+        {
+            get => _isRepeating;
+            set => _isRepeating = value;
+        }
+
+        //
+        // Summary:
+        //     Occurs when the timer interval has elapsed.
+        public event EventHandler Tick;
+
+
+        //
+        // Summary:
+        //     Starts the DispatcherTimer.
+        public void Start()
+        {
+            Stop();
+            var task = RunTimer();
+        }
+
+        //
+        // Summary:
+        //     Stops the DispatcherTimer.
+        public void Stop()
+        {
+            if (_cts != null)
+            {
+                try
+                {
+                    _cts.Cancel();
+                }
+                catch (ObjectDisposedException ex) { Console.WriteLine(ex.Message); }
+            }
+        }
+
+
+        async Task RunTimer()
+        {
+            _isEnabled = true;
+
+            try
+            {
+                using (_cts = new CancellationTokenSource())
+                using (var timer = new PeriodicTimer(_interval))
+                {
+                    var cancellationToken = _cts.Token;
+                    while (await timer.WaitForNextTickAsync(cancellationToken))
+                    {
+                        var task = OnTick(_isRepeating, cancellationToken);
+                    }
+                }
+            }
+            catch (TaskCanceledException ex) { Console.WriteLine(ex.Message); }
+            finally
+            {
+                _cts = null;
+                _isEnabled = false;
+            }
+        }
+
+        async Task OnTick(bool repeat, CancellationToken cancellationToken)
+        {
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                Tick?.Invoke(this, null);
+            }
+
+            if (!repeat)
+            {
+                Stop();
+            }
+        }
+    }
+}
